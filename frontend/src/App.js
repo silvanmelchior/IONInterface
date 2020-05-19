b745f92b552c2afc9a202eb4ae7244ed1478e76a
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-05-19 17:38:36 +0200

    init material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import axios from "axios";
+import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">Hello World</div>
+    <React.Fragment>
+      <h1>Test</h1>
+      <Button variant="contained" color="primary">Hello World</Button>
+    </React.Fragment>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
