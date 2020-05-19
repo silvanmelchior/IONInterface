1c7568447977b48f1d0993806d162c5fdc64cc1c
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-05-19 13:25:43 +0200

    osc argument send

--- osc.py
+++ osc.py
@@ -1,4 +1,5 @@
 import sys
+import struct
 import socket
 from config import ION_IP, ION_PORT
 
@@ -17,10 +18,39 @@ class OSCClient:
     def close(self):
         self.s.close()
 
-    def send_msg(self, msg):
+    def send_msg(self, msg, *args):
+        # message
         out = bytearray(msg.encode('ascii'))
         self._pad_inplace(out)
 
+        # arguments
+        if len(args) != 0:
+            out.extend(b',')
+            for arg in args:
+                if type(arg) == int:
+                    out.extend(b'i')
+                elif type(arg) == float:
+                    out.extend(b'f')
+                elif type(arg) == str:
+                    out.extend(b's')
+                else:
+                    raise ValueError
+            self._pad_inplace(out)
+            for arg in args:
+                if type(arg) == int:
+                    out.extend(arg.to_bytes(4, 'big', signed=True))
+                elif type(arg) == float:
+                    out.extend(struct.pack('>f', arg))
+                elif type(arg) == str:
+                    arg_bytes = bytearray(arg.encode('ascii'))
+                    arg_bytes.append(0)
+                    self._pad_inplace(arg_bytes, always_pad=False)
+                    out.extend(arg_bytes)
+                else:
+                    raise ValueError
+
+        # length
+        print('OUT', out)
         out_len = bytearray(len(out).to_bytes(4, 'big', signed=True))
         self.s.send(out_len + out)
 
@@ -33,8 +63,11 @@ class OSCClient:
                 return data
 
     @staticmethod
-    def _pad_inplace(buffer):
+    def _pad_inplace(buffer, always_pad=True):
         padding_len = 4 - len(buffer) % 4
+        if not always_pad:
+            if padding_len == 4:
+                padding_len = 0
         buffer.extend([0] * padding_len)
 
 
@@ -48,5 +81,8 @@ except (TimeoutError, ConnectionRefusedError):
 
 client.send_msg('/eos/ping')
 print(client.recv_msg('/eos/out/ping'))
+
+client.send_msg('/eos/ping', 5, 8., 'asdf')
+print(client.recv_msg('/eos/out/ping'))
+
 client.close()
-print('done')
