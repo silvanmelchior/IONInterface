089a68b28c49e6b926bba5a72fe21998cb2c8d7d
Silvan Melchior <silvan.melchior@zuehlke.com>
2020-05-25 11:34:16 +0200

    sub and cue first design

--- frontend/src/Cue.js
+++ frontend/src/Cue.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Divider, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 
 export default function Cue() {
   return (
     <>
-      <h1>Cue</h1>
+      <h1>Cue List 1</h1>
+      <List component="nav">
+        <ListItem button selected={false} onClick={event => console.log(event)}>
+          <ListItemIcon>10</ListItemIcon>
+          <ListItemText primary="&nbsp;" />
+        </ListItem>
+        <Divider />
+        <ListItem button selected={false} onClick={event => console.log(event)}>
+          <ListItemIcon>11</ListItemIcon>
+          <ListItemText primary="Einlass" />
+        </ListItem>
+        <Divider />
+        <ListItem button selected={false} onClick={event => console.log(event)}>
+          <ListItemIcon>11.1</ListItemIcon>
+          <ListItemText primary="Black" />
+        </ListItem>
+        <Divider />
+        <ListItem button selected={false} onClick={event => console.log(event)}>
+          <ListItemIcon>15</ListItemIcon>
+          <ListItemText primary="&nbsp;" />
+        </ListItem>
+        <Divider />
+        <ListItem button selected={false} onClick={event => console.log(event)}>
+          <ListItemIcon>30</ListItemIcon>
+          <ListItemText primary="&nbsp;" />
+        </ListItem>
+        <Divider />
+        <ListItem button selected={true} onClick={event => console.log(event)}>
+          <ListItemIcon>40</ListItemIcon>
+          <ListItemText primary="&nbsp;" />
+        </ListItem>
+        <Divider />
+        <ListItem button selected={false} onClick={event => console.log(event)}>
+          <ListItemIcon>50</ListItemIcon>
+          <ListItemText primary="&nbsp;" />
+        </ListItem>
+      </List>
     </>
   );
 }
--- frontend/src/Sub.js
+++ frontend/src/Sub.js
@@ -1,15 +1,77 @@
 import React from 'react';
+import { Box, Slider } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  sub: {
+    display: "inline-block",
+    width: 60
+  },
+  slider: {
+    height: 250
+  },
+  labelnum: {
+    opacity: 0.5
+  },
+  labelname: {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
+  }
+});
 
 export default function Sub() {
+
+  const classes = useStyles();
+
+  const [slider, setSlider] = React.useState(30);
+  const handleSlider = (event, newSlider) => {
+    setSlider(newSlider);
+  };
+
   return (
-    <>
-      <h1>Sub</h1>
-      <p>Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa </p>
-      <p>Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa </p>
-      <p>Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa </p>
-      <p>Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa </p>
-      <p>Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa </p>
-      <p>Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa Blaa </p>
-    </>
+    <Box>
+
+      <Box display="inline-block" textAlign="center" className={classes.sub} mt={8}>
+        <Box className={classes.slider}>
+          <Slider
+            orientation="vertical"
+            value={slider}
+            onChange={handleSlider}
+            min={0}
+            max={100}
+            step={1}
+            valueLabelDisplay="auto"
+          />
+        </Box>
+        <Box className={classes.labelnum} mt={1}>
+          1
+        </Box>
+        <Box className={classes.labelname}>
+          Front
+        </Box>
+      </Box>
+
+      <Box display="inline-block" textAlign="center" className={classes.sub} mt={8}>
+        <Box className={classes.slider}>
+          <Slider
+            orientation="vertical"
+            value={slider}
+            onChange={handleSlider}
+            min={0}
+            max={100}
+            step={1}
+            valueLabelDisplay="auto"
+          />
+        </Box>
+        <Box className={classes.labelnum} mt={1}>
+          2
+        </Box>
+        <Box className={classes.labelname}>
+          Back
+        </Box>
+      </Box>
+
+    </Box>
   );
 }
